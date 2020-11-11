9fab2173fdc74e611f6269873ff64dc0c170ebb1
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-11 17:22:35 +0200

    Finishing parse method

--- src/Parser.java
+++ src/Parser.java
@@ -18,7 +18,8 @@ public class Parser {
 			"date",
 			"help",
 			"pwd",
-			"clear"};
+			"clear",
+			"exit"};
 	
 	String[] spInput;
 
@@ -27,14 +28,45 @@ public class Parser {
 		// Split input to parse easily
 		spInput = input.split(" ");
 		
-		// Searching if it's a valid command
 		for(int i=0; i < allCmds.length; i++) {
 			if(allCmds[i].equals(spInput[0])) {
-				this.cmd = spInput[0];
-				return true;
+				cmd = spInput[0];
+				
+				if(cmd.equals("ls") || cmd.equals("more") || cmd.equals("args") ||
+						cmd.equals("date") || cmd.equals("help") || cmd.equals("pwd") ||
+						cmd.equals("clear") || cmd.equals("exit")) {
+					if(spInput.length - 1 == 0)
+						return true;
+				}
+				
+				else if(cmd.equals("cd") || cmd.equals("cat") || cmd.equals("mkdir") ||
+						cmd.equals("rmdir") || cmd.equals("rm")) {
+					if(spInput.length - 1 == 1) {
+						args = new String[1];
+						args[0] = spInput[1];
+						
+						return true;
+					}
+				}
+				
+				else if(cmd.equals("cp") || cmd.equals("mv")) {
+					if(spInput.length - 1 == 2) {
+						args = new String[2];
+						args[0] = spInput[1];
+						args[1] = spInput[2];
+						return true;
+					}
+				}
+				
+				else {
+					System.out.println("ERROR: WRONG ARGUMENTS!");
+					return false;
+				}
+				
 			}
 		}
 		
+		System.out.println("ERROR: UNKNOWN COMMAND!");
 		return false;
 	}
 	
