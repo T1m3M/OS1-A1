a4dca082f6e61e17f9b4b14d96026e6ffda89cc2
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-11 15:42:01 +0200

    Taking User input

--- src/main.java
+++ src/main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Parser parser = new Parser();
+		String userInput = ""; 
+		
+		while(true) {
+			// Taking user input
+			Scanner input = new Scanner(System.in);
+			System.out.print("$ ");
+			userInput = input.nextLine();
+			
+		}
 	}
 
 }
