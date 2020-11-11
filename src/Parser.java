7a8f4ceee681a11cde2f2ca24b1fb024087b4f3c
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-11 15:54:29 +0200

    Adding valid commands list

--- src/Parser.java
+++ src/Parser.java
@@ -2,4 +2,23 @@
 public class Parser {
 	String[] args;
 	String cmd;
+	
+	// All valid commands
+	public static String[] allCmds = {
+			"cd",
+			"ls",
+			"cp",
+			"cat",
+			"more",
+			"mkdir",
+			"rmdir",
+			"mv",
+			"rm",
+			"args",
+			"date",
+			"help",
+			"pwd",
+			"clear"};
+	
+	
 }
