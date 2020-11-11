a247e2c0446385bd9ab717f9a808908f441405b8
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-11 16:12:04 +0200

    Parsing if the command is valid

--- src/Parser.java
+++ src/Parser.java
@@ -20,5 +20,23 @@ public class Parser {
 			"pwd",
 			"clear"};
 	
+	String[] spInput;
+
+	public boolean parse(String input) {
+		
+		// Split input to parse easily
+		spInput = input.split(" ");
+		
+		// Searching if it's a valid command
+		for(int i=0; i < allCmds.length; i++) {
+			if(allCmds[i].equals(spInput[0])) {
+				this.cmd = spInput[0];
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	
 }
--- src/main.java
+++ src/main.java
@@ -5,13 +5,12 @@ public class main {
 	public static void main(String[] args) {
 		
 		Parser parser = new Parser();
-		String userInput = ""; 
 		
 		while(true) {
 			// Taking user input
 			Scanner input = new Scanner(System.in);
 			System.out.print("$ ");
-			userInput = input.nextLine();
+			parser.parse(input.nextLine());
 			
 		}
 	}
