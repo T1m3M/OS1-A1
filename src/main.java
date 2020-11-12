248d9062158b8d074153d39d3971d8e0aa7dffa4
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-12 12:11:19 +0200

    Adding IOException to the main class

--- src/main.java
+++ src/main.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Parser parser = new Parser();
 		Terminal terminal = new Terminal();
