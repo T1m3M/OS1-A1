0984ca9bd3b57555663213a341d0d08438e01d2d
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-12 08:49:25 +0200

    If valid command get the values

--- src/main.java
+++ src/main.java
@@ -5,12 +5,23 @@ public class main {
 	public static void main(String[] args) {
 		
 		Parser parser = new Parser();
+		String myCmd;
+		String[] myArgs;
 		
 		while(true) {
 			// Taking user input
 			Scanner input = new Scanner(System.in);
 			System.out.print("$ ");
-			parser.parse(input.nextLine());
+			
+			// checking if command and arguments are valid
+			if( parser.parse( input.nextLine() ) ) {
+				
+				// getting the command and its arguments values
+				myCmd = parser.getCmd();
+				myArgs = parser.getArguments();
+				
+				
+			}
 			
 		}
 	}
