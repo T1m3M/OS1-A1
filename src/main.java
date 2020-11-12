69535487f3c4f882b6566044ac0f05fafd904b94
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-12 09:35:15 +0200

    Adding Terminal object and identifying cases

--- src/main.java
+++ src/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		
 		Parser parser = new Parser();
+		Terminal terminal = new Terminal();
+		
 		String myCmd;
 		String[] myArgs;
 		
@@ -19,6 +21,29 @@ public class main {
 				// getting the command and its arguments values
 				myCmd = parser.getCmd();
 				myArgs = parser.getArguments();
+				
+				// Calling the specified terminal command
+				switch(myCmd) {
+				case "cd":
+					terminal.cd(myArgs[0]);
+					break;
+					
+				case "ls":
+					terminal.ls();
+					break;
+					
+				case "cat":
+					terminal.cat(myArgs);
+					break;
+					
+				case "rm":
+					terminal.rm(myArgs[0]);
+					break;
+					
+				case "pwd":
+					terminal.pwd();
+					break;
+				}
 				
 				
 			}
