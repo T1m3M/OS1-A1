b20b714dd4e872254f4258ebd5fbbd8f72b8955f
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-12 09:14:33 +0200

    Adding Terminal Interface

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,4 +1,22 @@
 
 public class Terminal {
-
+	public void cd(String path) {
+		
+	}
+	
+	public void ls() {
+		
+	}
+	
+	public void cat(String[] paths) {
+		
+	}
+	
+	public void rm(String sourcePath) {
+		
+	}
+	
+	public void pwd() {
+		
+	}
 }
