859d28aabad383782c454554e2fe54d3df0a99bb
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-12 12:12:10 +0200

    Adding ~ keyword for cd command and making paths canonical

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class Terminal {
@@ -6,30 +7,35 @@ public class Terminal {
 	private static String pwd = "C:\\"; // default
 	private static File dir = null;
 	
-	public void cd(String path) {
+	public void cd(String path) throws IOException {
+		
+		if(path.equals("~"))
+			pwd = "C:\\";
+		
+		else {
+			dir = new File(path);
 
-		dir = new File(path);
+			// Checking if path is relative or absolute
+			if( !(dir.isAbsolute()) )
+				dir = new File(pwd + "\\" + path);
 
-		// Checking if path is relative or absolute
-		if( !(dir.isAbsolute()) )
-			dir = new File(pwd + "\\" + path);
-
-		// Checking if it exists
-		if(dir.exists()) {
-			
-			// Checking if it's a directory and not a file
-			if(dir.isDirectory()) {
+			// Checking if it exists
+			if(dir.exists()) {
 				
-				// Updating the current working directory
-				pwd = dir.getAbsolutePath();
+				// Checking if it's a directory and not a file
+				if(dir.isDirectory()) {
+					
+					pwd = dir.getCanonicalPath();
+					
+				} else {
+					System.out.println("ERROR: only directories can be accessed!");
+				}
 				
 			} else {
-				System.out.println("ERROR: only directories can be accessed!");
-			}
-			
-		} else {
-			System.out.println("ERROR: no such file or directory!");
+				System.out.println("ERROR: no such file or directory!");
+			}	
 		}
+
 	}
 	
 	public void ls() {
